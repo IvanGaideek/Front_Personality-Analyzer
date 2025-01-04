866230979484c4f9db8fef35ae3bdad8c44f9e05
вашÐIvanGaideek <workgaideek37@gmail.com>
2025-01-05 02:12:14 +0300

    checkpoint1

--- src/components/organisms/DatabaseSearch.jsx
+++ src/components/organisms/DatabaseSearch.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchDatabaseComponent from '../molecules/SearchDatabaseComponent'
+import Table from './Table'
 
 export default function DatabaseSearch() {
 	return (
 		<div className='flex-container bg-all-black right-top-circle-bi'>
-			<SearchDatabaseComponent />
+			{/* <SearchDatabaseComponent /> */}
+			<Table />
 		</div>
 	)
 }
